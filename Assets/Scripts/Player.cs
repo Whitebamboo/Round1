0c8a76745f0cda6275f05918aca795500cd70eb1
hsuanju-wang <77294600+hsuanju-wang@users.noreply.github.com>
2021-09-18 22:53:24 -0400

    Add post processing
    Add post processing (greyscale) when player HP declines

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int maxHealth = 100;
-
+    [SerializeField] private int maxHealth = 90;
     public int currentHealth;
+
+    [SerializeField] private Volume volume;
+    [SerializeField] private VolumeProfile volumeProfile_1;
+    [SerializeField] private VolumeProfile volumeProfile_2;
+    [SerializeField] private VolumeProfile volumeProfile_3;
 
 
     // Start is called before the first frame update
@@ -19,7 +25,7 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        healthStatus();
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -27,7 +33,7 @@ public class Player : MonoBehaviour
         Debug.Log("collision");
         if (collision.gameObject.CompareTag("Nightmare"))
         {
-            TakeDamage(50);
+            TakeDamage(30);
             Destroy(collision.gameObject,0.9f);
         }
     }
@@ -35,7 +41,23 @@ public class Player : MonoBehaviour
     private void TakeDamage(int damage)
     {
         currentHealth -= damage;
-        if (currentHealth == 0)
+    }
+
+    private void healthStatus()
+    {
+        if (currentHealth == 90)
+        {
+            volume.profile = volumeProfile_1;
+        }
+        else if (currentHealth == 60)
+        {
+            volume.profile = volumeProfile_2;
+        }
+        else if (currentHealth == 30)
+        {
+            volume.profile = volumeProfile_3;
+        }
+        else if (currentHealth == 0)
         {
             SceneManager.LoadScene(1);
         }
