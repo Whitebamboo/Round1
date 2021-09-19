73a7d869552a871f58194d4a89aac007185edfef
hsuanju-wang <77294600+hsuanju-wang@users.noreply.github.com>
2021-09-18 22:55:23 -0400

    Add prologue video and start scene c script
    Add video and start scene c script

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    [SerializeField] private AudioSource bgm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgm.Play();
+        StartCoroutine("changeScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator changeScene()
+    {
+        yield return new WaitForSeconds(17.5f);
+        SceneManager.LoadScene(1);
+    }
+
+}
